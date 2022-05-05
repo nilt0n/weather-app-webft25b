7e7744113aa0e3ba371a9d71f940bbaac3b68d1f
nilt0n <info@nilton.org>
2022-05-05 10:09:19 -0500

    Aplicacion Clima

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//export default function Card(max,min,name,img,onClose) {
+  export default function Card(props) {
+  // acá va tu código
+ /* function handleOnClose() {
+    if(typeof onClose === "function") onClose();
+  }*/
+  return <div>Card Component
+   {/* <button onClick={handleOnClose}>XYZ</button>
+    <span>{name}</span>
+    <div>
+      <label >Min</label>
+      <span>{min}</span>
+</div> */}
+  </div>
+};
